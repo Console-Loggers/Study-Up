b499b6b9c53f35ed8bac06d328fab2fcd1c553c5
AKFosterDev <akfoster2015@gmail.com>
2021-01-25 13:20:28 -0800

    add complete functionality to show page

--- app/javascript/components/pages/DeckShow.js
+++ app/javascript/components/pages/DeckShow.js
@@ -1,4 +1,8 @@
 import React, { Component, Fragment } from 'react'
+import { Link } from 'react-router-dom'
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
+
+import Button from '../components/Button'
 
 export class DeckShow extends Component {
 	constructor(props) {
@@ -6,16 +10,46 @@ export class DeckShow extends Component {
 		this.state = {
 			cards: this.props.myCards,
 			index: 0,
-			isTerm: false,
+			isTerm: true,
 		}
 	}
 
-	onClick = () => {
-		if (this.state.class === 'term') {
-			this.setState({ class: 'definition' })
+	// Toggle between "Term" and "Definition"
+	onToggle = () => {
+		if (this.state.isTerm === true) {
+			this.setState({ isTerm: false })
 		} else {
-			this.setState({ class: 'term' })
+			this.setState({ isTerm: true })
 		}
+	}
+
+	prevClick = () => {
+		const { cards, index } = this.state
+		let newIndex = index - 1
+		let lastIndex = cards.length - 1
+		if (index === 0) {
+			this.setState({ index: lastIndex })
+		} else if (index > 0) {
+			this.setState({ index: newIndex })
+		}
+
+		// if index = 0 then setState index: this.state.cards.length
+
+		console.log('newIndex:', this.state.index)
+	}
+
+	nextClick = () => {
+		const { cards, index } = this.state
+		let newIndex = index + 1
+		if (index < cards.length - 1) {
+			console.log('cards length', cards.length)
+			console.log('index', index)
+			this.setState({ index: newIndex })
+		} else if (index === cards.length - 1) {
+			this.setState({ index: 0 })
+		}
+
+		console.log('newIndex:', this.state.index)
 	}
 
 	render() {
@@ -23,17 +57,32 @@ export class DeckShow extends Component {
 		console.log('state on show page:', this.state)
 		return (
 			<Fragment>
-				<div className='deck-title-card'>
-					{isTerm && (
-						<>
-							<h4>{cards[index].term}</h4>
-						</>
-					)}
-					{!isTerm && (
-						<>
-							<p>{cards[index].definition}</p>
-						</>
-					)}
+				<div className='show-card-container show-page'>
+					<div className='deck-show-card' onClick={this.onToggle}>
+						{isTerm && (
+							<>
+								<h4>{cards[index].term}</h4>
+							</>
+						)}
+						{!isTerm && (
+							<>
+								<p>{cards[index].definition}</p>
+							</>
+						)}
+					</div>
+					<div className='arrow-container'>
+						<button className='prev-arrow'>
+							<FaArrowLeft size={25} onClick={this.prevClick} />
+						</button>
+						<button className='next-arrow'>
+							<FaArrowRight size={25} onClick={this.nextClick} />
+						</button>
+					</div>
+					<Link to='/mydecks'>
+						<Button className='button outline-button sm-button'>
+							<span>Go Back</span>
+						</Button>
+					</Link>
 				</div>
 			</Fragment>
 		)
