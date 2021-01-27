7c6a0a2f21d0386c83e9418b516936fbe696cd3f
AlanHausinger <alanh621@gmail.com>
2021-01-27 14:17:27 -0800

    working on styling the homepage

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import Button from '../components/Button'
+import { GiBookshelf, GiWhiteBook, GiBookmarklet } from 'react-icons/gi'
 
 export class Home extends Component {
 	render() {
@@ -16,6 +17,36 @@ export class Home extends Component {
 							<Button className='button-outline-button'>
 								<span>Login</span>
 							</Button>
+						</div>
+					</div>
+					<div className='home-content-container'>
+						<div className='home-content-text'>
+							<h3>
+								We are study up and this is just a for example and hopefully it
+								looks good lmao
+							</h3>
+						</div>
+						<div className='icon-container'>
+							<div className='icon-position1'>
+								<GiBookshelf size={35} className='icon-styling' />
+								<h1>
+									Step 1:
+									<br />
+									Create a Deck
+								</h1>
+							</div>
+							<GiWhiteBook className='icon-styling' />
+							<h1>
+								Step 2:
+								<br />
+								Add Cards
+							</h1>
+							<GiBookmarklet className='icon-styling' />
+							<h1>
+								Step 3:
+								<br />
+								StudyUp!
+							</h1>
 						</div>
 					</div>
 				</div>
