01cb0bcb97dfbdc5b975bb475af89e79cf9b3c60
AKFosterDev <akfoster2015@gmail.com>
2021-01-27 10:38:39 -0800

    add team data to js file

---
+++ app/javascript/aboutData.js
@@ -0,0 +1,34 @@
+let team = [
+	{
+		id: 1,
+		name: 'Matt Ramirez',
+		role: 'Project Manager',
+		bio:
+			'Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters.',
+		image: './app/assets/images/matt.png',
+	},
+	{
+		id: 2,
+		name: 'Amanda Foster',
+		role: 'Tech Lead',
+		bio:
+			'Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters.',
+		image: './app/assets/images/amanda.png',
+	},
+	{
+		id: 3,
+		name: 'Chris Morton',
+		role: 'Communications Lead',
+		bio:
+			'Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters.',
+		image: './app/assets/images/chris.jpg',
+	},
+	{
+		id: 4,
+		name: 'Alan Hausinger',
+		role: 'Design Lead',
+		bio:
+			'Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters.',
+		image: './app/assets/images/alan.png',
+	},
+]
