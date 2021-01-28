2df1ed4a23caf201c42d710dd4045454a46ad845
AKFosterDev <akfoster2015@gmail.com>
2021-01-28 09:37:28 -0800

    change up home styling

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import Button from '../components/Button'
-import { GiBookshelf, GiWhiteBook, GiBookmarklet } from 'react-icons/gi'
+import { BiPencil, BiSmile, BiGame, BiLandscape } from 'react-icons/bi'
 
 export class Home extends Component {
 	render() {
@@ -22,32 +22,11 @@ export class Home extends Component {
 					<div className='home-content-container'>
 						<div className='home-content-text'>
 							<h3>
-								We are study up and this is just a for example and hopefully it
-								looks good lmao
+								Create fully customizable flashcards for anything you need to
+								study! Conveniently access from anywhere!
 							</h3>
 						</div>
-						<div className='icon-container'>
-							<div className='icon-position1'>
-								<GiBookshelf size={35} className='icon-styling' />
-								<h1>
-									Step 1:
-									<br />
-									Create a Deck
-								</h1>
-							</div>
-							<GiWhiteBook className='icon-styling' />
-							<h1>
-								Step 2:
-								<br />
-								Add Cards
-							</h1>
-							<GiBookmarklet className='icon-styling' />
-							<h1>
-								Step 3:
-								<br />
-								StudyUp!
-							</h1>
-						</div>
+						<div className='icon-container'></div>
 					</div>
 				</div>
 			</Fragment>
