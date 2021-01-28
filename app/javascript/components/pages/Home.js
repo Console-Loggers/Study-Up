dca2f7c339d4b7b8b6f64ae5bd46014f05021d3b
AKFosterDev <akfoster2015@gmail.com>
2021-01-28 13:05:17 -0800

    Add links to buttons, work on icons

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react'
+import { Link } from 'react-router-dom'
 import Button from '../components/Button'
 import { BiPencil, BiSmile, BiGame, BiLandscape } from 'react-icons/bi'
 
@@ -10,15 +11,20 @@ export class Home extends Component {
 					<div className='hero-container'>
 						<h1 className='home-title'>Welcome to Study Up</h1>
 						<div className='button-container'>
-							<Button className='button-outline-button'>
-								<span>Sign Up</span>
-							</Button>
+							<Link>
+								<Button className='button-outline-button'>
+									<span>Sign Up</span>
+								</Button>
+							</Link>
 
-							<Button className='button-outline-button'>
-								<span>Login</span>
-							</Button>
+							<Link>
+								<Button className='button-outline-button'>
+									<span>Login</span>
+								</Button>
+							</Link>
 						</div>
 					</div>
+
 					<div className='home-content-container'>
 						<div className='home-content-text'>
 							<h3>
@@ -26,7 +32,35 @@ export class Home extends Component {
 								study! Conveniently access from anywhere!
 							</h3>
 						</div>
-						<div className='icon-container'></div>
+
+						<div className='icon-container'>
+							<div className='icon-group'>
+								<div className='icon'>
+									<BiPencil size={50} />
+								</div>
+								<div className='icon-text'>
+									<p>Fully Customizable</p>
+								</div>
+							</div>
+
+							<div className='icon-group'>
+								<div className='icon'>
+									<BiSmile size={50} />
+								</div>
+								<div className='icon-text'>
+									<p>Easy to Use</p>
+								</div>
+							</div>
+
+							<div className='icon-group'>
+								<div className='icon'>
+									<BiLandscape size={50} />
+								</div>
+								<div className='icon-text'>
+									<p>On The Go</p>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</Fragment>
