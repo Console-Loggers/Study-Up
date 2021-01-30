d2225e2944dac8827f2f288f43d218af9019733b
AKFosterDev <akfoster2015@gmail.com>
2021-01-30 05:16:59 -0800

    add styles to home page

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,5 +1,14 @@
 import React, { Component, Fragment } from 'react'
-import { Link } from 'react-router-dom'
+import {
+	Card,
+	CardImg,
+	CardTitle,
+	CardText,
+	CardDeck,
+	CardSubtitle,
+	CardBody,
+} from 'reactstrap'
+
 import Button from '../components/Button'
 import { BiPencil, BiSmile, BiGame, BiLandscape } from 'react-icons/bi'
 
@@ -11,17 +20,17 @@ export class Home extends Component {
 					<div className='hero-container'>
 						<h1 className='home-title'>Welcome to Study Up</h1>
 						<div className='button-container'>
-							<Link>
-								<Button className='button-outline-button'>
+							<a href='/users/sign_up'>
+								<Button className='button white-solid-button'>
 									<span>Sign Up</span>
 								</Button>
-							</Link>
+							</a>
 
-							<Link>
-								<Button className='button-outline-button'>
+							<a href='/users/sign_in'>
+								<Button className='button  blue-solid-button'>
 									<span>Login</span>
 								</Button>
-							</Link>
+							</a>
 						</div>
 					</div>
 
@@ -34,32 +43,26 @@ export class Home extends Component {
 						</div>
 
 						<div className='icon-container'>
-							<div className='icon-group'>
-								<div className='icon'>
-									<BiPencil size={50} />
-								</div>
-								<div className='icon-text'>
-									<p>Fully Customizable</p>
-								</div>
-							</div>
-
-							<div className='icon-group'>
-								<div className='icon'>
-									<BiSmile size={50} />
-								</div>
-								<div className='icon-text'>
-									<p>Easy to Use</p>
-								</div>
-							</div>
-
-							<div className='icon-group'>
-								<div className='icon'>
-									<BiLandscape size={50} />
-								</div>
-								<div className='icon-text'>
-									<p>On The Go</p>
-								</div>
-							</div>
+							<CardDeck>
+								<Card className='m-5 home-card'>
+									<BiPencil className='icon' size={55} />
+									<CardBody>
+										<CardTitle tag='h4'>Fully Customizable</CardTitle>
+									</CardBody>
+								</Card>
+								<Card className='m-5 home-card'>
+									<BiSmile className='icon' size={55} />
+									<CardBody>
+										<CardTitle tag='h4'>Easy to Use</CardTitle>
+									</CardBody>
+								</Card>
+								<Card className='m-5 home-card'>
+									<BiLandscape className='icon' size={55} />
+									<CardBody>
+										<CardTitle tag='h4'>Use Anywhere</CardTitle>
+									</CardBody>
+								</Card>
+							</CardDeck>
 						</div>
 					</div>
 				</div>
