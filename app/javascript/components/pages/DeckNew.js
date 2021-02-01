003858a18c0a6a5f7b59f34af1f2c440d518b01c
M-ram224 <mattramirez2011@gmil.com>
2021-02-01 10:07:52 -0800

    start on new deck form

--- app/javascript/components/pages/DeckNew.js
+++ app/javascript/components/pages/DeckNew.js
@@ -1,126 +1,111 @@
-import React, { Component, Fragment } from 'react'
-import { Redirect } from 'react-router-dom'
-import TermCard from '../components/TermCard'
-import Button from '../components/Button'
+import React, { Component, Fragment } from "react"
+import { Redirect } from "react-router-dom"
+import TermCard from "../components/TermCard"
+import Button from "../components/Button"
+import { Form, FormGroup, Label, Input, FormText } from "reactstrap"
 
 export class DeckNew extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			form: {
-				title: '',
-				description: '',
-				cards: [
-					{
-						term: '',
-						definition: '',
-					},
-					{
-						term: '',
-						definition: '',
-					},
-					{
-						term: '',
-						definition: '',
-					},
-				],
-			},
-			submitted: false,
-		}
-	}
+  constructor(props) {
+    super(props)
+    this.state = {
+      form: {
+        title: "",
+        description: "",
+        cards: [
+          {
+            term: "",
+            definition: "",
+          },
+          {
+            term: "",
+            definition: "",
+          },
+          {
+            term: "",
+            definition: "",
+          },
+        ],
+      },
+      submitted: false,
+    }
+  }
 
-	handleChange = (e) => {
-		let { form } = this.state
-		form[e.target.name] = e.target.value
-		this.setState({ form: form })
-	}
+  handleChange = (e) => {
+    let { form } = this.state
+    form[e.target.name] = e.target.value
+    this.setState({ form: form })
+  }
 
-	handleTermChange = (e, cardNumber) => {
-		const { cards } = this.state.form
-		let { form } = this.state
-		let currentCards = cards
-		let newCards = [...currentCards]
-		newCards[cardNumber] = { term: e.target.value, definition: '' }
-		form.cards = newCards
-		this.setState({ form: form })
-	}
+  handleTermChange = (e, cardNumber) => {
+    const { cards } = this.state.form
+    let { form } = this.state
+    let currentCards = cards
+    let newCards = [...currentCards]
+    newCards[cardNumber] = { term: e.target.value, definition: "" }
+    form.cards = newCards
+    this.setState({ form: form })
+  }
 
-	handleDefChange = (e, cardNumber) => {
-		const { cards } = this.state.form
-		let { form } = this.state
-		let currentCards = cards
-		let newCards = [...currentCards]
-		const thisCard = cards[cardNumber]
-		newCards[cardNumber] = { ...thisCard, definition: e.target.value }
-		form.cards = newCards
-		this.setState({ form: form })
-	}
+  handleDefChange = (e, cardNumber) => {
+    const { cards } = this.state.form
+    let { form } = this.state
+    let currentCards = cards
+    let newCards = [...currentCards]
+    const thisCard = cards[cardNumber]
+    newCards[cardNumber] = { ...thisCard, definition: e.target.value }
+    form.cards = newCards
+    this.setState({ form: form })
+  }
 
-	handleAddCard = () => {
-		const { cards } = this.state.form
-		let { form } = this.state
-		form.cards = [...cards, { term: '', definition: '' }]
-		this.setState({ form: form })
-	}
+  handleAddCard = () => {
+    const { cards } = this.state.form
+    let { form } = this.state
+    form.cards = [...cards, { term: "", definition: "" }]
+    this.setState({ form: form })
+  }
 
-	handleSubmit = (e) => {
-		e.preventDefault()
-		this.props.createDeck(this.state.form)
-		this.setState({ submitted: true })
-	}
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.createDeck(this.state.form)
+    this.setState({ submitted: true })
+  }
 
-	render() {
-		const { form } = this.state
-		return (
-			<Fragment>
-				<h1>Create a New Deck</h1>
-				<form>
-					<label> Title </label>
-					<input
-						type='text'
-						name='title'
-						value={form.title}
-						onChange={this.handleChange}
-						placeholder='Enter a title for this deck.'
-					/>
+  render() {
+    const { form } = this.state
+    return (
+      <Fragment>
+        <h1>Create a New Deck</h1>
+        <Form>
+          <FormGroup>
+            <Label for="exampleEmail">Email</Label>
+            <Input
+              type="email"
+              name="email"
+              id="exampleEmail"
+              placeholder="with a placeholder"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="examplePassword">Password</Label>
+            <Input
+              type="password"
+              name="password"
+              id="examplePassword"
+              placeholder="password placeholder"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleText">Text Area</Label>
+            <Input type="textarea" name="text" id="exampleText" />
+          </FormGroup>
 
-					<label> Description </label>
-					<input
-						type='text'
-						name='description'
-						value={form.description}
-						onChange={this.handleChange}
-						placeholder='Enter a description for this deck.'
-					/>
-					{this.state.form.cards.map((card, index) => (
-						<TermCard
-							key={index}
-							cardNumber={index}
-							termChange={this.handleTermChange}
-							defChange={this.handleDefChange}
-						/>
-					))}
-					<Button
-						type='button'
-						className='button outline-button'
-						onClick={() => this.handleAddCard()}
-					>
-						Add a Card
-					</Button>
-					<Button
-						type='submit'
-						className='button outline-button'
-						onClick={this.handleSubmit}
-						name='submit'
-					>
-						Create Deck
-					</Button>
-				</form>
+          <Button>Submit</Button>
+        </Form>
 
-				{this.state.submitted && <Redirect to='/mydecks' />}
-			</Fragment>
-		)
-	}
+        {this.state.submitted && <Redirect to="/mydecks" />}
+      </Fragment>
+    )
+  }
 }
 
 export default DeckNew
