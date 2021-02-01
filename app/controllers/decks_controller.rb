e58ffb07c1e3f5270fcc7e6ea45e424742ee49ec
H. Michael Ko <hyosunko@gmail.com>
2021-02-01 15:19:52 -0800

    Add card_attributes to createDeck to match rails' expected params name

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -4,22 +4,22 @@ class DecksController < ApplicationController
         decks = Deck.where(user_id: current_user.id)
         render json: decks.to_json(include: :cards)
     end
-    
+
     def show
         deck = Deck.find(params[:id])
         render json: deck.to_json(include: :cards)
     end
 
     def create
-        #deck_params[:user_id]=current_user.id
-        deck = Deck.create(deck_params)
-        if deck.valid?
+        deck = current_user.decks.new(deck_params)
+
+        if deck.save
             render json: deck
-        else 
+        else
             render json: deck.errors, status: :unprocessable_entity
         end
     end
-    
+
     def update
         deck = Deck.find(params[:id])
         deck.update(deck_params)
@@ -39,13 +39,14 @@ class DecksController < ApplicationController
             render json: deck.errors, status: :unprocessable_entity
         end
     end
-    
+
     private
     def deck_params
-        params.require(:deck).permit(:title, :description, :user_id, card_attributes: [ :term, :definition, :deck_id])
+        params.require(:deck).permit(
+            :title,
+            :description,
+            :user_id,
+            cards_attributes: [ :term, :definition, :deck_id]
+        )
     end
-
-    # def card_params
-    #     params.require(:cards).permit(card_attributes: [ :term, :definition, :deck_id])
-    # end
 end
