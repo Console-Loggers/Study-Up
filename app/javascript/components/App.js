6d11e51aa5a97008414a5e3ab8bc1214b1d6b26c
M-ram224 <mattramirez2011@gmil.com>
2021-02-02 12:04:48 -0800

    definition button work

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,214 +1,215 @@
-import React, { Component, Fragment } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React, { Component, Fragment } from "react"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
-import Home from './pages/Home'
-import About from './pages/About'
-import NotFound from './pages/NotFound'
-import DeckIndex from './pages/DeckIndex'
-import DeckNew from './pages/DeckNew'
-import DeckShow from './pages/DeckShow'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import NotFound from "./pages/NotFound"
+import DeckIndex from "./pages/DeckIndex"
+import DeckNew from "./pages/DeckNew"
+import DeckNewVocab from "./pages/DeckNewVocab"
+import DeckShow from "./pages/DeckShow"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 // import decks from "../mockDecks.js"
 // import cards from "../mockCards.js"
 
 class App extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			decks: [],
-		}
-	}
+  constructor(props) {
+    super(props)
+    this.state = {
+      decks: [],
+    }
+  }
 
-	componentDidMount() {
-		this.deckIndex()
-	}
+  componentDidMount() {
+    this.deckIndex()
+  }
 
-	deckIndex = () => {
-		fetch('/decks')
-			.then((response) => {
-				return response.json()
-			})
-			.then((decks) => {
-				this.setState({ decks: decks })
-			})
-			.catch((error) => {
-				console.log('index errors:', error)
-			})
-	}
+  deckIndex = () => {
+    fetch("/decks")
+      .then((response) => {
+        return response.json()
+      })
+      .then((decks) => {
+        this.setState({ decks: decks })
+      })
+      .catch((error) => {
+        console.log("index errors:", error)
+      })
+  }
 
-	createDeck = (newDeck) => {
-		console.log('newDeck:', newDeck)
-		let userId = this.props.current_user.id
-		newDeck.user_id = userId
-		newDeck.cards_attributes = newDeck.cards.filter(
-			(card) => card.term !== '' && card.definition !== ''
-		)
-		fetch('/decks', {
-			body: JSON.stringify(newDeck),
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			method: 'POST',
-		})
-			.then((response) => {
-				console.log('create deck response:', response)
-				if (response.status === 422) {
-					alert('Please Check Submission.')
-				}
+  createDeck = (newDeck) => {
+    console.log("newDeck:", newDeck)
+    let userId = this.props.current_user.id
+    newDeck.user_id = userId
+    newDeck.cards_attributes = newDeck.cards.filter(
+      (card) => card.term !== "" && card.definition !== ""
+    )
+    fetch("/decks", {
+      body: JSON.stringify(newDeck),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    })
+      .then((response) => {
+        console.log("create deck response:", response)
+        if (response.status === 422) {
+          alert("Please Check Submission.")
+        }
 
-				return response.json()
-			})
-			.then((payload) => {
-				newDeck.cards.forEach((card) => {
-					this.createCard(card, payload.id)
-				})
-				this.deckIndex()
-				console.log('payload:', payload)
-			})
-			.catch((errors) => {
-				console.log('create errors', errors)
-			})
-	}
-	createCard = (newCard, deckId) => {
-		newCard.deck_id = deckId
-		fetch('/cards', {
-			body: JSON.stringify(newCard),
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			method: 'POST',
-		})
-			.then((response) => {
-				console.log('create card response:', response)
-				if (response.status === 422) {
-					alert('Please Check Submission.')
-				}
-				return response.json()
-			})
-			.then((payload) => {
-				console.log('payload')
-			})
-	}
+        return response.json()
+      })
+      .then((payload) => {
+        newDeck.cards.forEach((card) => {
+          this.createCard(card, payload.id)
+        })
+        this.deckIndex()
+        console.log("payload:", payload)
+      })
+      .catch((errors) => {
+        console.log("create errors", errors)
+      })
+  }
+  createCard = (newCard, deckId) => {
+    newCard.deck_id = deckId
+    fetch("/cards", {
+      body: JSON.stringify(newCard),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    })
+      .then((response) => {
+        console.log("create card response:", response)
+        if (response.status === 422) {
+          alert("Please Check Submission.")
+        }
+        return response.json()
+      })
+      .then((payload) => {
+        console.log("payload")
+      })
+  }
 
-	// updateDeck = (updateDeck, id) => {}
+  // updateDeck = (updateDeck, id) => {}
 
-	deleteDeck = (id) => {
-		return fetch(`/decks/${id}`, {
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			method: 'DELETE',
-		})
-			.then((response) => {
-				return response.json()
-			})
-			.then(() => {
-				this.deckIndex()
-			})
-			.catch((errors) => {
-				console.log('delete errors:', errors)
-			})
-	}
+  deleteDeck = (id) => {
+    return fetch(`/decks/${id}`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "DELETE",
+    })
+      .then((response) => {
+        return response.json()
+      })
+      .then(() => {
+        this.deckIndex()
+      })
+      .catch((errors) => {
+        console.log("delete errors:", errors)
+      })
+  }
 
-	render() {
-		// console.log('decks in state', this.state)
+  render() {
+    // console.log('decks in state', this.state)
 
-		const {
-			logged_in,
-			current_user,
-			sign_in_route,
-			sign_out_route,
-		} = this.props
+    const {
+      logged_in,
+      current_user,
+      sign_in_route,
+      sign_out_route,
+    } = this.props
 
-		const { decks, cards } = this.state
+    const { decks, cards } = this.state
 
-		return (
-			<Fragment>
-				<div className='app'>
-					<Header
-						loggedIn={logged_in}
-						signIn={sign_in_route}
-						signOut={sign_out_route}
-					/>
+    return (
+      <Fragment>
+        <div className="app">
+          <Header
+            loggedIn={logged_in}
+            signIn={sign_in_route}
+            signOut={sign_out_route}
+          />
 
-					<Router>
-						<Switch>
-							{/* ----- Home ----- */}
-							{/* {!logged_in &&  />} */}
-							<Route exact path='/' component={Home} />
+          <Router>
+            <Switch>
+              {/* ----- Home ----- */}
+              {/* {!logged_in &&  />} */}
+              <Route exact path="/" component={Home} />
 
-							{/* ----- About ----- */}
-							<Route path='/about' component={About} />
+              {/* ----- About ----- */}
+              <Route path="/about" component={About} />
 
-							{/* ----- Protected Deck Index ----- */}
-							{logged_in && (
-								<Route
-									path='/mydecks'
-									render={(props) => {
-										return (
-											<DeckIndex decks={decks} deleteDeck={this.deleteDeck} />
-										)
-									}}
-								/>
-							)}
+              {/* ----- Protected Deck Index ----- */}
+              {logged_in && (
+                <Route
+                  path="/mydecks"
+                  render={(props) => {
+                    return (
+                      <DeckIndex decks={decks} deleteDeck={this.deleteDeck} />
+                    )
+                  }}
+                />
+              )}
 
-							{/* ----- Protected Deck Show ----- */}
-							{logged_in && (
-								<Route
-									path='/mydeck/:id'
-									render={(props) => {
-										console.log(props)
-										const id = props.match.params.id
-										let deck = decks.find((deck) => deck.id === parseInt(id))
-										if (!deck) {
-											return <h6>Loading...</h6>
-										}
-										let myCards = deck.cards
-										console.log('app js show route', myCards)
-										return decks.length > 0 && <DeckShow myCards={myCards} />
-									}}
-								/>
-							)}
+              {/* ----- Protected Deck Show ----- */}
+              {logged_in && (
+                <Route
+                  path="/mydeck/:id"
+                  render={(props) => {
+                    console.log(props)
+                    const id = props.match.params.id
+                    let deck = decks.find((deck) => deck.id === parseInt(id))
+                    if (!deck) {
+                      return <h6>Loading...</h6>
+                    }
+                    let myCards = deck.cards
+                    console.log("app js show route", myCards)
+                    return decks.length > 0 && <DeckShow myCards={myCards} />
+                  }}
+                />
+              )}
 
-							{/* ----- Protected Deck New ----- */}
-							<Route
-								path='/decknew'
-								render={(props) => {
-									return (
-										<DeckNew
-											createDeck={this.createDeck}
-											current_user={current_user}
-										/>
-									)
-								}}
-							/>
-							{/* ----- Protected Deck Vocab New ----- */}
-							<Route
-								path='/decknewvocab'
-								render={(props) => {
-									return (
-										<DeckNewVocab
-											createDeck={this.createDeck}
-											current_user={current_user}
-										/>
-									)
-								}}
-							/>
+              {/* ----- Protected Deck New ----- */}
+              <Route
+                path="/decknew"
+                render={(props) => {
+                  return (
+                    <DeckNew
+                      createDeck={this.createDeck}
+                      current_user={current_user}
+                    />
+                  )
+                }}
+              />
+              {/* ----- Protected Deck Vocab New ----- */}
+              <Route
+                path="/decknewvocab"
+                render={(props) => {
+                  return (
+                    <DeckNewVocab
+                      createDeck={this.createDeck}
+                      current_user={current_user}
+                    />
+                  )
+                }}
+              />
 
-							{/* ----- Protected Deck Edit ----- */}
-							{/* <Route path='/deckedit/:id' component={DeckEdit} /> */}
+              {/* ----- Protected Deck Edit ----- */}
+              {/* <Route path='/deckedit/:id' component={DeckEdit} /> */}
 
-							{/* ----- Not Found ----- */}
-							<Route component={NotFound} />
-						</Switch>
-					</Router>
-					<Footer />
-				</div>
-			</Fragment>
-		)
-	}
+              {/* ----- Not Found ----- */}
+              <Route component={NotFound} />
+            </Switch>
+          </Router>
+          <Footer />
+        </div>
+      </Fragment>
+    )
+  }
 }
 
 export default App
--- app/javascript/components/pages/DeckNewVocab.js
+++ app/javascript/components/pages/DeckNewVocab.js
@@ -1,157 +1,160 @@
-import React, { Component, Fragment } from 'react'
-import { Redirect } from 'react-router-dom'
-import TermCard from '../components/TermCard'
-import Button from '../components/Button'
-import { Form, FormGroup, Label, Input, FormText, Container } from 'reactstrap'
+import React, { Component, Fragment } from "react"
+import { Redirect } from "react-router-dom"
+import TermCard from "../components/TermCard"
+import Button from "../components/Button"
+import { Form, FormGroup, Label, Input, FormText, Container } from "reactstrap"
 
 export class DeckNewVocab extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			form: {
-				title: '',
-				description: '',
-				cards: [],
-				query: '',
-				definition: '',
-			},
-			submitted: false,
-		}
-	}
+  constructor(props) {
+    super(props)
+    this.state = {
+      form: {
+        title: "",
+        description: "",
+        cards: [],
+        query: "",
+        definition: "",
+      },
+      submitted: false,
+    }
+  }
 
-	handleChange = (e) => {
-		let { form } = this.state
-		form[e.target.name] = e.target.value
-		this.setState({ form: form })
-	}
+  handleChange = (e) => {
+    let { form } = this.state
+    form[e.target.name] = e.target.value
+    this.setState({ form: form })
+  }
 
-	handleTermChange = (e, cardNumber) => {
-		const { query } = this.state.form
-		let { form } = this.state
-		// let currentCards = cards
-		// let newCards = [...currentCards]
-		// newCards[cardNumber] = { term: e.target.value, definition: '' }
-		// form.cards = newCards
-		this.setState({ form: form })
-	}
+  handleTermChange = (e, cardNumber) => {
+    const { query } = this.state.form
+    let { form } = this.state
+    // let currentCards = cards
+    // let newCards = [...currentCards]
+    // newCards[cardNumber] = { term: e.target.value, definition: '' }
+    // form.cards = newCards
+    this.setState({ form: form })
+  }
 
-	setDefinitionCard = (meaning, cardNumber) => {
-		const { cards } = this.state.form
-		let { form } = this.state
-		let currentCards = cards
-		let newCards = [...currentCards]
-		const thisCard = cards[cardNumber]
-		newCards[cardNumber] = { ...thisCard, definition: meaning }
-		form.cards = newCards
-		this.setState({ form: form })
-	}
+  setDefinitionCard = (meaning, cardNumber) => {
+    const { cards } = this.state.form
+    let { form } = this.state
+    let currentCards = cards
+    let newCards = [...currentCards]
+    const thisCard = cards[cardNumber]
+    newCards[cardNumber] = { ...thisCard, definition: meaning }
+    form.cards = newCards
+    this.setState({ form: form })
+  }
 
-	handleAddCard = () => {
-		const { cards } = this.state.form
-		let { form } = this.state
-		form.cards = [...cards, { term: '', definition: '' }]
-		this.setState({ form: form })
-	}
+  handleAddCard = () => {
+    const { cards } = this.state.form
+    let { form } = this.state
+    form.cards = [...cards, { term: "", definition: "" }]
+    this.setState({ form: form })
+  }
 
-	handleSubmit = (e) => {
-		e.preventDefault()
-		this.props.createDeck(this.state.form)
-		this.setState({ submitted: true })
-	}
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.createDeck(this.state.form)
+    this.setState({ submitted: true })
+  }
 
-	getDefinition = (term, cardNum) => {
-		const { form, definition } = this.state
-		const api = `https://twinword-word-graph-dictionary.p.rapidapi.com/definition/?entry=${term}`
-		fetch(api, {
-			headers: {
-				'x-rapidapi-key': process.env.REACT_APP_API_KEY,
-				'x-rapidapi-host': 'twinword-word-graph-dictionary.p.rapidapi.com',
-			},
-			method: 'GET',
-		})
-			.then((response) => {
-				console.log(response)
-				return response.json()
-			})
-			.then((payload) => {
-				setDefinitionCard(payload.meaning, cardNum)
-			})
-			.catch((err) => {
-				console.error(err)
-			})
-	}
+  getDefinition = (term, cardNum) => {
+    const { form, definition } = this.state
+    console.log({ term })
+    const api = `https://twinword-word-graph-dictionary.p.rapidapi.com/definition/?entry=${term}`
+    fetch(api, {
+      headers: {
+        "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+        "x-rapidapi-host": "twinword-word-graph-dictionary.p.rapidapi.com",
+      },
+      method: "GET",
+    })
+      .then((response) => {
+        console.log(response)
+        return response.json()
+      })
+      .then((payload) => {
+        console.log("payload :>> ", payload)
+        this.setDefinitionCard(payload.meaning, cardNum)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
 
-	render() {
-		const { form } = this.state
-		return (
-			<Fragment>
-				<Container>
-					<h1>Create a Vocab Deck</h1>
-					<Form>
-						<FormGroup>
-							<Label>Title</Label>
-							<Input
-								type='text'
-								name='title'
-								value={form.title}
-								onChange={this.handleChange}
-								placeholder='Enter a title for this deck.'
-							/>
-						</FormGroup>
-						<FormGroup>
-							<Label>Description</Label>
-							<Input
-								type='text'
-								name='description'
-								value={form.description}
-								onChange={this.handleChange}
-								placeholder='Enter a description for this deck.'
-							/>
-						</FormGroup>
-						<FormGroup>
-							<Label>Term</Label>
-							<Input
-								type='text'
-								name='term'
-								value={form.cards[0].term}
-								onChange={this.handleChange}
-								placeholder='Enter a vocab word for this card.'
-							/>
-							<Button
-								type='submit'
-								className='button blue-outline-button sm-button'
-								onSubmit={() => this.getDefinition(form.cards[0].term, 0)}
-							>
-								<span>Search</span>
-							</Button>
-						</FormGroup>
+  render() {
+    const { form } = this.state
+    // console.log(form)
+    return (
+      <Fragment>
+        <Container>
+          <h1>Create a Vocab Deck</h1>
+          <Form>
+            <FormGroup>
+              <Label>Title</Label>
+              <Input
+                type="text"
+                name="title"
+                value={form.title}
+                onChange={this.handleChange}
+                placeholder="Enter a title for this deck."
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label>Description</Label>
+              <Input
+                type="text"
+                name="description"
+                value={form.description}
+                onChange={this.handleChange}
+                placeholder="Enter a description for this deck."
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label>Term</Label>
+              <Input
+                type="text"
+                name="query"
+                value={form.query}
+                onChange={this.handleChange}
+                placeholder="Enter a vocab word for this card."
+              />
+              <Button
+                type="button"
+                className="button blue-outline-button sm-button"
+                onClick={() => this.getDefinition(form.query, 0)}
+              >
+                <span>Search</span>
+              </Button>
+            </FormGroup>
 
-						<div className='add-button-container'>
-							<Button
-								type='button'
-								className='button blue-outline-button sm-button'
-								onClick={() => this.handleAddCard()}
-							>
-								<span>Add a Card</span>
-							</Button>
-						</div>
-					</Form>
+            <div className="add-button-container">
+              <Button
+                type="button"
+                className="button blue-outline-button sm-button"
+                onClick={() => this.handleAddCard()}
+              >
+                <span>Add a Card</span>
+              </Button>
+            </div>
+          </Form>
 
-					<div className='create-button-container'>
-						<Button
-							type='submit'
-							className='button blue-solid-button'
-							onClick={this.handleSubmit}
-							name='submit'
-						>
-							<span>Create Deck</span>
-						</Button>
-					</div>
-				</Container>
-				{this.state.submitted && <Redirect to='/mydecks' />}
-			</Fragment>
-		)
-	}
+          <div className="create-button-container">
+            <Button
+              type="submit"
+              className="button blue-solid-button"
+              onClick={this.handleSubmit}
+              name="submit"
+            >
+              <span>Create Deck</span>
+            </Button>
+          </div>
+        </Container>
+        {this.state.submitted && <Redirect to="/mydecks" />}
+      </Fragment>
+    )
+  }
 }
 
 export default DeckNewVocab
